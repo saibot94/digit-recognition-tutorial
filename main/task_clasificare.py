e2514dd289b317ed5e0479db977d11e26ba5b772
Cristian Schuszter <cschuszter@metabiota.com>
2016-12-19 19:47:50 +0200

    Initial commit

---
+++ main/task_clasificare.py
@@ -0,0 +1,34 @@
+from sklearn.externals import joblib
+from skimage.feature import hog
+import numpy as np
+
+import cv2
+
+clf = joblib.load('digits_clf.pkl')
+print '=> Clasificatorul a fost incarcat'
+
+im = cv2.imread('digit-reco.jpg')
+im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+im_gray = cv2.GaussianBlur(im_gray, ksize=(5, 5), sigmaX=0)
+
+ret, image_threshold = cv2.threshold(im_gray, 91, 255, cv2.THRESH_BINARY_INV)
+contours, hierarchy = cv2.findContours(image_threshold.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+rectangles = [cv2.boundingRect(ctr) for ctr in contours]
+for rect in rectangles:
+    # Desenare dreptunghi
+    cv2.rectangle(im, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (0, 255, 0), 3)
+
+    leng = int(rect[3] * 1.5)
+    pt1 = int(rect[1] + rect[3] // 2 - leng // 2)
+    pt2 = int(rect[0] + rect[2] // 2 - leng // 2)
+    region_of_interest = image_threshold[pt1:pt1 + leng, pt2:pt2 + leng]
+    roi = cv2.resize(region_of_interest, (28, 28), interpolation=cv2.INTER_AREA)
+    roi = cv2.dilate(roi, (3, 3))
+
+    fd = hog(roi, orientations=9, pixels_per_cell=(14, 14), cells_per_block=(1, 1), visualise=False)
+    nbr = clf.predict(np.array([fd], 'float64'))
+    cv2.putText(im, str(int(nbr[0])), (rect[0], rect[1]), cv2.FONT_HERSHEY_DUPLEX, 2, (0, 255, 255), 3)
+
+cv2.imshow("Final classification", im)
+cv2.waitKey()
---
+++ main/train_classifier.py
@@ -0,0 +1,25 @@
+from sklearn.externals import joblib
+from sklearn import datasets
+from skimage.feature import hog
+from sklearn.svm import LinearSVC
+import numpy as np
+
+dataset = datasets.fetch_mldata("MNIST Original")
+
+imagini = np.array(dataset.data, 'int16')
+etichete = np.array(dataset.target, 'int')
+
+print '=> Creare lista de trasaturi din fiecare imagine...'
+list_hog_fd = []
+for imagine in imagini:
+    fd = hog(imagine.reshape(28, 28), orientations=9, pixels_per_cell=(14, 14), cells_per_block=(1, 1), visualise=False)
+    list_hog_fd.append(fd)
+
+computed_features = np.array(list_hog_fd, 'float64')
+clf = LinearSVC()
+
+print '=> Antrenare clasificator...'
+clf.fit(computed_features, etichete)
+joblib.dump(clf, "digits_clf.pkl", compress=3)
+
+print '=> Am terminat!'
